2e94658dda1418381267bab4c28d15bd446ba120
const-ae <artjom31415@googlemail.com>
2018-10-29 16:44:45 +0100

    Implement fit_global_dropout_curves

--- R/fit_dropout_curves.R
+++ R/fit_dropout_curves.R
@@ -15,9 +15,24 @@
 #'   but all have a common scale}
 #' }
 #'
-fit_global_dropout_curves <- function(){
+fit_global_dropout_curves <- function(X, mup, sigma2p, experimental_design,
+                                      prev_zeta, prev_rho, maxit=5000){
 
+    sigmoid_est <- optim(par=c(zeta=prev_zeta[1], rho=prev_rho[1]), function(par){
+        if(par[1]  > 0) return(Inf)
+        - sum(vapply(seq_len(ncol(X)), function(colidx){
+            sum(invprobit(X[, colidx], par[2], par[1], oneminus = TRUE, log=TRUE), na.rm=TRUE) +
+            sum(invprobit(mup[which(is.na(c(X[, colidx]))), experimental_design[colidx]], par[2],
+                    par[1] * sqrt(1 + sigma2p[which(is.na(c(X[, colidx])))] / par[1]^2), log=TRUE))
+        }, FUN.VALUE = 0.0))
+    }, control=list(maxit=maxit))
+    rho <- rep(sigmoid_est$par["rho"], ncol(X))
+    zeta <- rep(sigmoid_est$par["zeta"], ncol(X))
+    names(rho) <- NULL
+    names(zeta) <- NULL
+    list(zeta=zeta, rho=rho, converged=sigmoid_est$convergence == 0)
 }
+
 
 #' @rdname fit_global_dropout_curves
 fit_sample_dropout_curves <- function(){
@@ -27,7 +42,7 @@ fit_sample_dropout_curves <- function(){
 #' @rdname fit_global_dropout_curves
 fit_global_scale_dropout_curves <- function(X, mup, sigma2p, experimental_design,
                              prev_zeta, prev_rho,
-                             max_iter=10, epsilon=1e-7){
+                             max_iter=10, epsilon=1e-5){
 
     zeta <- mean(prev_zeta)
     rho <- prev_rho
@@ -40,8 +55,8 @@ fit_global_scale_dropout_curves <- function(X, mup, sigma2p, experimental_design
             if(zeta > 0) return(1e100) # Aka. Inf, but Inf triggers a warning
             -sum(vapply(seq_len(ncol(X)), function(colidx){
                 sum(invprobit(X[, colidx], rho[colidx], zeta, log=TRUE, oneminus = TRUE), na.rm=TRUE) +
-                    sum(invprobit(mup[which(is.na(c(X[, colidx]))), experimental_design[colidx]],
-                         rho[colidx], zeta * sqrt(1 + sigma2p[which(is.na(c(X[, colidx])))] / zeta^2), log=TRUE))
+                   sum(invprobit(mup[which(is.na(c(X[, colidx]))), experimental_design[colidx]],
+                     rho[colidx], zeta * sqrt(1 + sigma2p[which(is.na(c(X[, colidx])))] / zeta^2), log=TRUE))
             }, FUN.VALUE = 0.0))
 
         }, p=zeta, stepmax=1)$estimate
@@ -49,8 +64,8 @@ fit_global_scale_dropout_curves <- function(X, mup, sigma2p, experimental_design
         rho <- sapply(seq_len(ncol(X)), function(colidx){
             nlm(function(rho){
                 -(sum(invprobit(X[, colidx], rho, zeta, log=TRUE, oneminus = TRUE), na.rm=TRUE) +
-                      sum(invprobit(mup[which(is.na(c(X[, colidx]))), experimental_design[colidx]],
-                         rho, zeta * sqrt(1 + sigma2p[which(is.na(c(X[, colidx])))] / zeta^2), log=TRUE)))
+                   sum(invprobit(mup[which(is.na(c(X[, colidx]))), experimental_design[colidx]],
+                     rho, zeta * sqrt(1 + sigma2p[which(is.na(c(X[, colidx])))] / zeta^2), log=TRUE)))
             }, p=rho[colidx])$estimate
         })
 
--- tests/testthat/test-fit_dropout_curves.R
+++ tests/testthat/test-fit_dropout_curves.R
@@ -1,5 +1,47 @@
 context("test-fit_dropout_curves")
 
+
+
+
+
+
+test_that("fitting a global sigmoids work", {
+    set.seed(1)
+    rho <- rep(19, times=5)
+    zeta <- rep(-0.5, times=5)
+    data <- generate_synthetic_data(n_rows = 1000, n_replicates=5,
+                                    rho=rho, zeta=zeta)
+    expect_silent({
+        res <- fit_global_dropout_curves(data$X, data$mus, data$sigmas2,
+                       experimental_design=rep(1, 5), prev_rho=rnorm(5, 18, 1),
+                       prev_zeta=-1)
+    })
+    expect_true(res$converged)
+    expect_equal(res$rho, rho, tolerance=0.1)
+    expect_equal(res$zeta, zeta, tolerance=0.1)
+
+    # More data means better fit
+    set.seed(1)
+    rho <- rep(18.7, times=10)
+    zeta <- rep(-0.5, times=10)
+    data <- generate_synthetic_data(n_rows = 1e4, n_replicates=10,
+                                    rho=rho, zeta=zeta)
+    expect_silent({
+        res <- fit_global_dropout_curves(data$X, data$mus, data$sigmas2,
+                       experimental_design=rep(1, 10), prev_rho=18,
+                       prev_zeta=-1)
+    })
+    expect_true(res$converged)
+    expect_equal(res$rho, rho, tolerance=0.01)
+    expect_equal(res$zeta, zeta, tolerance=0.01)
+})
+
+
+
+
+
+
+
 test_that("fitting a global scale and individual rhos work", {
     set.seed(1)
     rho <- rnorm(5, mean=19, sd=1)
