722b3147d7305deeffbf768c716ac9fab26e9c1b
const-ae <artjom31415@googlemail.com>
2018-12-08 16:04:59 +0100

    Implement function to transform prodd_parameter object

--- R/prodd_parameters.R
+++ R/prodd_parameters.R
@@ -3,8 +3,19 @@
 # This file provides a few useful helper function for handling the prodd_parameter function
 
 is_valid_prodd_parameters <- function(x, ...){
-    ! is.null(x$converged) &&  ! is.null(x$error) && ! is.null(x$hyper_params) &&
+    base_elements_present <- ! is.null(x$converged) &&  ! is.null(x$error) && ! is.null(x$hyper_params) &&
         ! is.null(x$feature_params) && ! is.null(x$experimental_design)
+    hyper_elements_present <- ! is.null(x$hyper_params$mu0) && ! is.null(x$hyper_params$sigma20) &&
+        ! is.null(x$hyper_params$rho) && ! is.null(x$hyper_params$zeta) &&
+        ! is.null(x$hyper_params$nu) && ! is.null(x$hyper_params$eta)
+    feature_elements_present <- ! is.null(x$feature_params$mup) && ! is.null(x$feature_params$sigma2mup) &&
+        ! is.null(x$feature_params$sigma2p)
+    feature_elements_right_size <- is.matrix(x$feature_params$mup) && is.matrix(x$feature_params$sigma2mup) &&
+        nrow(x$feature_params$mup) == nrow(x$feature_params$sigma2mup) &&
+        ncol(x$feature_params$mup) == ncol(x$feature_params$sigma2mup) &&
+        length(x$feature_params$sigma2p) == nrow(x$feature_params$mup) &&
+        length(unique(x$experimental_design)) == ncol(x$feature_params$mup)
+    base_elements_present && hyper_elements_present && feature_elements_present && feature_elements_right_size
 }
 
 #' @rdname print.prodd_parameters
@@ -50,3 +61,75 @@ print.prodd_parameters <- function(x, ...){
 
 
 is.prodd_parameters <- function(x) inherits(x, "prodd_parameters")
+
+
+#' Update the experimental_design of a parameter object
+#'
+#' Helper method to transform the parameters from one experimental_design
+#' to another. It avoids expensive re-calculation of the parameters and
+#' instead uses a averaging of the parameters to produce the new values.
+#'
+#' The method only works for changing the condition assignment of samples
+#' or removing samples. The method cannot add new samples and infer the
+#' parameters for them.
+#'
+#' The method is for example useful if you want to calculate the distance
+#' with unbiased feature_parameters that don't already include the information
+#' to which condition a sample belongs.
+#'
+#' @param params an object of class `prodd_parameters` which is returned by the
+#'   \code{fit_parameters()} function.
+#' @param new_experimental_design a vector that assignes each sample to one condition.
+#'   It has to have the same length as the old experimental_design. It can either be
+#'   a factor, a character or a numeric vector. Each unique element is one condition.
+#'   To remove samples code them as \code{NA}.
+#' @return the updated list of class prodd_parameters
+#'
+#' @export
+transform_parameters <- function(params, new_experimental_design){
+
+    stopifnot(is.prodd_parameters(params))
+    stopifnot(is_valid_prodd_parameters(params))
+    stopifnot(length(new_experimental_design) == length(params$experimental_design))
+
+    new_experimental_design_fct <- as.factor(new_experimental_design)
+    new_experimental_design <- as.numeric(new_experimental_design_fct)
+    N_cond <- length(levels(new_experimental_design_fct)) # To make sure to ignore NA's
+
+    old_experimental_design <- params$experimental_design
+
+    helper_mup <- params$feature_params$mup[ ,old_experimental_design, drop=FALSE]
+    new_mup <- t(mply_dbl(seq_len(N_cond), ncol=nrow(helper_mup), function(condition){
+        rowMeans(helper_mup[, which(new_experimental_design == condition),drop=FALSE])
+    }))
+
+    helper_sigma2mup <- params$feature_params$sigma2mup[ ,old_experimental_design, drop=FALSE]
+    new_sigma2mup <- t(mply_dbl(seq_len(N_cond), ncol=nrow(helper_mup), function(condition){
+        rowMeans(helper_sigma2mup[, which(new_experimental_design == condition),drop=FALSE])
+    }))
+
+    colnames(new_mup) <- levels(new_experimental_design_fct)
+    colnames(new_sigma2mup) <- levels(new_experimental_design_fct)
+    rownames(new_mup) <- rownames(params$feature_params$mup)
+    rownames(new_sigma2mup) <- rownames(params$feature_params$sigma2mup)
+
+
+    hyper_params <- params$hyper_params
+    hyper_params$rho <- hyper_params$rho[! is.na(new_experimental_design)]
+    hyper_params$zeta <- hyper_params$zeta[! is.na(new_experimental_design)]
+
+    ret <- list(hyper_params = hyper_params,
+                feature_params = list(
+                    mup=new_mup,
+                    sigma2p=params$feature_params$sigma2p,
+                    sigma2mup=new_sigma2mup
+                ),
+                experimental_design=new_experimental_design_fct[! is.na(new_experimental_design)],
+                error=params$error, converged=params$converged)
+
+    class(ret) <- "prodd_parameters"
+    ret
+}
+
+
+
--- tests/testthat/test-prodd_parameters.R
+++ tests/testthat/test-prodd_parameters.R
@@ -2,18 +2,27 @@ context("test-prodd_parameters")
 
 test_that("printing looks beautiful", {
     n_rows <- 100
-    n_cols <- 10
-    experimental_design <- rep(1:2, each=n_cols/2)
+    n_cols <- 6
+    n_conditions <- 2
+    experimental_design <- rep(1:n_conditions, each=n_cols/n_conditions)
     hyper_params <- list(eta=0.2234234, nu=3.31213, mu0=18, sigma20=3.123, rho=rnorm(n_cols, mean=18), zeta=rep(-1, times=n_cols))
-    feat_params <- list(mup=matrix(rnorm(n_cols * n_rows, mean=18), nrow=n_rows),
+    feat_params <- list(mup=matrix(rnorm(n_conditions * n_rows, mean=18), nrow=n_rows, ncol=n_conditions),
                         sigma2p=rep(0.4, times=n_rows),
-                        sigma2mup = matrix(0.1, nrow=n_rows, ncol=n_cols))
+                        sigma2mup = matrix(0.1, nrow=n_rows, ncol=n_conditions))
     params <- list(hyper_params=hyper_params,
                    feature_params=feat_params,
                    experimental_design=experimental_design,
                    error=1e-5, converged=TRUE)
     class(params) <- "prodd_parameters"
     params
+
+    expect_true(is_valid_prodd_parameters(params))
+
+    trans_params <- transform_parameters(params, c(1, 3, 2, 2, NA, 1))
+    expect_equal(trans_params$feature_params$mup[, "3"], params$feature_params$mup[, 1])
+    expect_equal(trans_params$experimental_design, factor(c(1, 3, 2, 2, 1)))
+    expect_equal(trans_params$error, params$error)
+    expect_equal(trans_params$feature_params$mup[, "1"], trans_params$feature_params$mup[, "2"])
 
     nrep <- 2
     mu0 <- 20
